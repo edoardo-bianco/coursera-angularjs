e6316f4d9695638be1736b5320051bfce438f55f
Edoardo Bianco <Edoardo Bianco>
2016-10-26 07:51:14 -0200

    First web app angular js file

---
+++ app.js
@@ -0,0 +1,11 @@
+/*IIFE Immediately-invoked function expression*/
+(function(){
+  'use strict'
+
+  angular.module('myFirstApp',[]);
+  //define the viewmodel of our view index.html
+  //we have bound our javascript
+  .controller('MyFirstController', function(){
+    //define the functionality for that particular controller
+  })
+})();
